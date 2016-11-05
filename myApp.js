b56f709caf3302888b088f86479910993756d61e
Abdeljabar <aitbelkhalil@gmail.com>
2016-11-05 21:18:49 +0000

    Fist commit

---
+++ myApp.js
@@ -0,0 +1,2 @@
+
+var app= angular.module('myApp',[]); //on defini le module
---
+++ myCtrl.js
@@ -0,0 +1,6 @@
+
+var app= angular.module('myApp',[]); //on defini le module
+app.controller('myController', function($scope){ //puis on asossier un controller a notre module
+  $scope.prenom="abdeljabar";
+  $scope.nom="belkhalil";
+});
